3689f015d8a4c95af492d697144aef8e565a75cb
gordon macgregor <grmacgregor@gmail.com>
2016-05-21 11:24:43 -0600

    finished loop prac 1

---
+++ loops.js
@@ -0,0 +1,43 @@
+// 1.
+
+for(var i=0; i<600; i+=100) {
+	console.log(i);
+};
+
+// 2.
+
+for(var i=1; i<128; i*=2) {
+	console.log(i);
+};
+
+// 3.
+
+for(var i=1; i<4; i++) {
+	console.log(i + "\n" + i + "\n" + i);
+}
+
+// 4.
+
+for(var i=0; i<12; i+=2) {
+	console.log(i);
+};
+
+// 5.
+
+for(var i=3; i<18; i+=3) {
+	console.log(i);
+};
+
+// 6.
+
+for(var i=9; i>=0; i--) {
+	console.log(i);
+};
+
+// 7.
+
+for(var i=0; i<3; i++) {
+	for(var j=0; j<4 ; j++) {
+		console.log(j);
+	}
+}
